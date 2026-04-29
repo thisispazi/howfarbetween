a914b4db7d276d116a999270bd72e87c93192dc9
Patrick Reiter <office@iqoon.at>
2026-04-29 21:02:02 +0200

    Add edge runtime for Cloudflare Pages

--- app/distance/[route]/page.tsx
+++ app/distance/[route]/page.tsx
@@ -1,3 +1,4 @@
+export const runtime = 'edge'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { CITIES, getDistanceKm, getTravelTimes, formatDuration, crossesOcean, getCityBySlug, getNearbyCities } from '../../lib/cities'

1c1a3f91d080383fed77edd75d23ca9d06860061
Patrick Reiter <office@iqoon.at>
2026-05-03 11:48:23 +0200

    Setup for Cloudflare Workers with OpenNext

--- app/distance/[route]/page.tsx
+++ app/distance/[route]/page.tsx
@@ -1,5 +1,3 @@
-export const runtime = 'edge'
-
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { CITIES, getDistanceKm, getTravelTimes, formatDuration, crossesOcean, getCityBySlug, getNearbyCities } from '../../lib/cities'
